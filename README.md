57ddda3fb536ee0985a7232564ec764f311bd7b3
pi0treq92 <32812870+pi0treq92@users.noreply.github.com>
2020-02-12 23:09:01 +0100

    pdg generator

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Django_Online_Shop
+### The main puprpose was create web application using Django 2.2.8 framework
+### Technologies used in project:
+
+<p><a href="url"><img src="https://docs.celeryproject.org/en/latest/_static/celery_512.png" align="left" height="80"></a>
+&emsp;&emsp;&emsp;&emsp;&emsp;<a href="url"><img src="https://cloud.githubusercontent.com/assets/72164/2638484/3f743636-beaf-11e3-8f26-0d3b41f12edf.png" align="left" height="64"></a></p>
+<p><a href="url"><img src="https://weasyprint.readthedocs.io/en/stable/_static/logo.png" align="left" height="64"></a> &emsp;&emsp;<a href="url"><img src="https://www.rabbitmq.com/img/RabbitMQ-logo.svg" align="left"></a></p>
--- onlineshop/orders/views.py
+++ onlineshop/orders/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, get_object_or_404
+from .models import OrderItem, Order
 from .forms import OrderForm
 from basket.basket import Basket
 from .tasks import created_order
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
+
+@staff_member_required
+def order_to_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="order_{}.pdf"'.format(order.id)
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.MEDIA_ROOT + 'css/pdf.css')])
+    return response
+
 
 def create_order(request):
     basket = Basket(request)
